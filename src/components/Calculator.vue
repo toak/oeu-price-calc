5fea58295d1fa8e3aa3f70685af74689fe06becf
toak <info@toak.de>
2019-01-21 21:23:23 +0100

    added calculator

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -13,17 +13,39 @@
         <td>Druck</td>
         <td>Preis</td>
       </tr>
-      <tr v-for="print in prints">
+      <tr v-for="(print, key) in prints" :key="key">
         <td>
-          <select v-model="print.size" v-bind:value="print.size">
-            <option v-for="(value, key) in prices.print">{{ key }}</option>
+          <select
+            v-model="print.size"
+            v-bind:value="print.size"
+            v-on:change="calcPrintPrice(print);"
+          >
+            <option v-for="(value, key) in prices.print" :key="key">{{
+              key
+            }}</option>
           </select>
         </td>
-        <td><input></input></td>
-        <td>Druck</td>
-        <td>Preis</td>
+        <td>
+          <input
+            v-model="print.number"
+            size="4"
+            v-on:keyup="calcPrintPrice(print);"
+          />
+        </td>
+        <td>
+          <select
+            v-model="print.double"
+            v-bind:value="print.double"
+            v-on:change="calcPrintPrice(print);"
+          >
+            <option value="false">Einseitig</option>
+            <option value="true">Doppelseitig</option>
+          </select>
+        </td>
+        <td>{{ print.price }} Euro</td>
       </tr>
     </table>
+
     <h2>Preiskalkulation</h2>
     <table>
       <tr>
@@ -43,8 +65,10 @@
         <td>Euro</td>
       </tr>
       <tr>
-        <td>Summe:</td>
-        <td>{{ priceSum }} Euro</td>
+        <td><b>Summe:</b></td>
+        <td>
+          <b>{{ priceSum }} Euro</b>
+        </td>
       </tr>
     </table>
   </div>
@@ -61,46 +85,14 @@ export default {
         shippingStandard: 10,
         shippingChurch: 5,
         print: {
-          A0: {
-            A4a: 16,
-            priceA4: 0.1,
-            price: 1.6
-          },
-          A1: {
-            A4a: 8,
-            priceA4: 0.1,
-            price: 0.8
-          },
-          A2: {
-            A4a: 4,
-            priceA4: 0.1,
-            price: 0.4
-          },
-          A3: {
-            A4a: 2,
-            priceA4: 0.1,
-            price: 0.2
-          },
-          A4: {
-            A4a: 1,
-            priceA4: 0.02,
-            price: 0.02
-          },
-          A5: {
-            A4a: 0.5,
-            priceA4: 0.02,
-            price: 0.01
-          },
-          A6: {
-            A4a: 0.25,
-            priceA4: 0.02,
-            price: 0.01
-          },
-          A7: {
-            A4a: 0.13,
-            priceA4: 0.02,
-            price: 0
-          }
+          A0: 1.6,
+          A1: 0.8,
+          A2: 0.4,
+          A3: 0.2,
+          A4: 0.02,
+          A5: 0.01,
+          A6: 0.005,
+          A7: 0.0025
         }
       },
       settings: {
@@ -111,12 +103,27 @@ export default {
         0: {
           size: "A4",
           number: 100,
-          double: false
+          double: false,
+          basePrice: 0,
+          price: 0
         }
       }
     };
   },
-  methods: {},
+  methods: {
+    calcPrintPrice: function(e) {
+      if (e.size === "A0") e.basePrice = this.prices.print.A0;
+      if (e.size === "A1") e.basePrice = this.prices.print.A1;
+      if (e.size === "A2") e.basePrice = this.prices.print.A2;
+      if (e.size === "A3") e.basePrice = this.prices.print.A3;
+      if (e.size === "A4") e.basePrice = this.prices.print.A4;
+      if (e.size === "A5") e.basePrice = this.prices.print.A5;
+      if (e.size === "A6") e.basePrice = this.prices.print.A6;
+      if (e.size === "A7") e.basePrice = this.prices.print.A7;
+      if (e.double === true) e.price = e.basePrice * e.number * 2;
+      else e.price = e.basePrice * e.number;
+    }
+  },
   computed: {
     priceSum: function() {
       return (
